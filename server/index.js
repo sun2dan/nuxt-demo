7f4a8a3efe0e04799f333bb7f8fdff2984506aa8
sun2dan <superfiresun@163.com>
2020-05-29 00:20:47 +0800

    1. 实现功能：node代理(页面ajax)接口； 方案1.1：在服务器 server/index 中添加逻辑，用 koa-router 库实现代理转发

--- server/index.js
+++ server/index.js
@@ -2,47 +2,58 @@ const Koa = require('koa')
 const consola = require('consola')
 const {Nuxt, Builder} = require('nuxt')
 const serverAxios = require('./serverAxios');
+const Router = require('koa-router'); // koa 路由中间件
 
 const app = new Koa()
+const router = new Router(); // 实例化路由
 
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
 config.dev = app.env !== 'production'
 
+// 代理接口
+router.get('/proxy/*', async (ctx, next) => {
+  let link = ctx.request.url.replace(/\/proxy/gmi, '');
+  let res = await serverAxios.get(link);
+  ctx.response.body = res.data;
+});
+app.use(router.routes());
+
 async function start() {
   // Instantiate nuxt.js
   const nuxt = new Nuxt(config)
-
+  
   const {
     host = process.env.HOST || '127.0.0.1',
     port = process.env.PORT || 3000
   } = nuxt.options.server
-
+  
   await nuxt.ready()
   // Build in development
   if (config.dev) {
     const builder = new Builder(nuxt)
     await builder.build()
   }
-
+  
   app.use(async (ctx) => {
-    let url = ctx.request.url;
-
+    
+    /*let url = ctx.request.url;
+    
     if (/\/proxy\//gmi.test(url)) {  // 需要本机代理的ajax接口，来源：页面ajax异步调用
       let link = url.replace(/\/proxy/gmi, '');
       let res = await serverAxios.get(link);
       ctx.response.body = res.data;
       return;
-    }
-
+    }*/
+    
     ctx.status = 200
     ctx.respond = false // Bypass Koa's built-in response handling
     ctx.req.ctx = ctx // This might be useful later on, e.g. in nuxtServerInit or with nuxt-stash
     nuxt.render(ctx.req, ctx.res)
   })
-
+  
   app.listen(port, host)
-
+  
   consola.ready({
     message: `Server listening on http://${host}:${port}`,
     badge: true
