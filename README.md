ee72e72104c89f5c7c894b7c5054731e1a7fb9e3
sunchao <supericesun@gmail.com>
2020-05-28 14:46:55 +0800

    0. 普通ajax，请求数据并渲染

--- README.md
+++ README.md
@@ -1,22 +1,41 @@
 # nuxt-demo
 
-> 
+## 功能
+### 1. node代理/转发接口
+#### 使用场景
+- 接口没有做跨域处理，页面直接发送ajax请求会报跨域的错；
+- 接口不能直接调用，需要先获取一个token，然后带着token去获取数据；
 
-## Build Setup
+#### 设计思路
+三种方案：
+1. 在node服务器代码中，添加过滤，只要是当前页面发送过来的ajax请求，做统一转发；
+2. serverMiddleware方案：没有node服务器（创建项目时没有选择server framework）或不想动 server/index.js 的代码时，可以使用服务器中间件serverMiddleware来处理；
+3. [@nuxtjs/proxy](https://www.npmjs.com/package/@nuxtjs/proxy)方案，但只能做单纯的转发，解决跨域问题，不能实现带token请求的复杂逻辑   
 
-```bash
-# install dependencies
-$ yarn install
 
-# serve with hot reload at localhost:3000
-$ yarn dev
 
-# build for production and launch server
-$ yarn build
-$ yarn start
+## 接口
 
-# generate static project
-$ yarn generate
-```
+### 接口基本信息
+在线mock平台：https://www.fastmock.site/
 
-For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
+接口地址前缀：https://www.fastmock.site/mock/26c97e7a6aaac3d8e356c15412b5abc3/api/
+
+### 获取新闻推荐接口
+- news/recommend
+- 参数：无
+- 返回数据：
+```json
+{
+  "code": 1,
+  "data": [{
+    "title": "Zoom停止中国个人用户注册，不再提供免费服务",
+    "publishTime": "2020-05-19 10:03:10",
+    "origin": "网易新闻"
+  }]
+}
+``` 
+
+
+
+
--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <div class="floor f-banner">
       <div class="main-box">
         <h2>banner楼层</h2>
+        <div>{{msg}}</div>
       </div>
     </div>
     <div class="floor f-tab">
@@ -13,6 +14,18 @@
     <div class="floor f-news">
       <div class="main-box">
         <h2>新闻楼层</h2>
+        <ul class="flex">
+          <li class="flex-1" v-for="(item,i) in recmdList">
+            <div class="img-box">
+              <img :src="item.img" class="abs-mm" alt="">
+            </div>
+            <div class="info-box flex">
+              <span>{{item.origin}}</span>
+              <span>{{item.publishTime}}</span>
+            </div>
+            <h4 class="ellips">{{item.title}}</h4>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -26,11 +39,34 @@
     layout: 'default',
     head: tkd.index,
     data() {
-      return {}
+      return {
+        recmdList: [],
+        msg: ''
+      }
+    },
+    async asyncData() {
+
     },
     created() {
+      this.getNews();
     },
-    methods: {},
+    methods: {
+      async getNews() {
+        let res1 = await pageAxios.get("news/recommend", {params: {}});
+        let code = res1.code;
+        let recmdList = res1.data;
+        this.recmdList = recmdList;
+        console.log(code, recmdList);
+      },
+      // 本地测试 跨域问题
+      async getCont() {
+        let res1 = await pageAxios.get("http://test.jd.com:8092", {params: {}});
+        let code = res1.code;
+        let data = res1.data;
+        console.log('cont', res1, code, data);
+        return res1;
+      },
+    },
     computed: {},
     components: {}
   }
@@ -39,6 +75,16 @@
   .page-index {
     .f-banner {color: $f; background: #FFB200;}
     .f-tab {background: #E4E4E4;}
-    .f-news {color: $f; background: $main;}
+    .f-news {color: $f; background: $main;
+      ul {margin-top: 30px;}
+      li {width: 370px; margin-right: 45px;}
+      .img-box {height: 200px; }
+      .info-box {margin: 15px 0 10px; color: $minorF;
+        span:last-of-type {position: relative; margin-left: 10px; padding-left: 10px;
+          &:before {content: ''; position: absolute; left: -1px; top: 0; bottom: 0; width: 2px; background-color: $borderF;}
+        }
+      }
+      h4 {font-size: 18px; line-height: 1.5;}
+    }
   }
 </style>
