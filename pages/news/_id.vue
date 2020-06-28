c3fb24e22aea677ee2e377793347865e8c774df3
sunchao <supericesun@gmail.com>
2020-06-28 17:38:25 +0800

    refactor: 修改结构

--- pages/news/_id.vue
+++ pages/news/_id.vue
@@ -1,25 +1,29 @@
 <template>
-  <div class="page-newsdetail">
-    
+  <div>
+
   </div>
 </template>
+
 <script>
   export default {
     name: '',
     props: {
+      arg: String
     },
     data() {
       return {}
     },
     created() {
     },
+    mounted() {
+    },
+    watch: {},
     methods: {},
     computed: {},
     components: {}
   }
 </script>
+
 <style rel="stylesheet/scss" lang="scss" type="text/scss">
-  .page-newsdetail {
-    
-  }
+
 </style>
